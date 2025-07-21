de3caf4e1e69233de74b21a1d31ddd638acedd1f
Elian Rehbani <elianrhbn@gmail.com>
2025-07-21 17:32:07 -0300

    deleted: i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
-// @ts-check
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  
   vite: {
     plugins: [tailwindcss()],
   },
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['en', 'es', 'fr', 'kr'],
-    routing: {
-      prefixDefaultLocale: true
-    }
-  },
-
-  integrations: [react()],
-});
+  integrations: [
+    react(),
+  ],
+})
