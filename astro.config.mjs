251b968df41de26b48f6270e69044c9e9271fcb6
Elian Rehbani <elianrhbn@gmail.com>
2025-07-21 17:14:18 -0300

    delete: SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({}),
   
   vite: {
     plugins: [tailwindcss()],
-    // ssr: {
-    //   // Externalizar dependencias que pueden causar problemas
-    //   external: ["@astrojs/react", "@vercel/analytics", "@vercel/speed-insights"],
-    // },
   },
   
   i18n: {
